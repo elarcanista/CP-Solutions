c2263f6a66c26243ee7ea54a824d85a28dcc905c
Andrés Ortega <elarcanista@gmail.com>
2016-03-11 18:55:20 -0500

    500A - New Year Transportation

---
+++ CodeForces/500A.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	int n, t, act;
+	cin >> n;
+	cin >> t;
+	--n;
+	bool yes = false;
+	int i = 1;
+	while(i <= n){
+		cin >> act;
+		int next = i + act;
+		if(next == t){
+			yes = true;
+			break;
+		}
+		if(i + act > t) break;
+		while(++i < next){
+			cin >> act;
+		}
+	}
+	if(yes) cout << "YES\n";
+	else cout << "NO\n";
+	return 0;
+}
