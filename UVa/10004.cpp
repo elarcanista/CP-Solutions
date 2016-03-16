0f0af4d3578933ca0a3d09b67ef7cb781d161907
Andrés Ortega <elarcanista@gmail.com>
2016-03-16 00:59:01 -0500

    10004 - Bicoloring

---
+++ UVa/10004.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int MAXV = 199;
+vector<int> g [MAXV];
+int color[MAXV];
+
+void cleangraph (){
+	for(int i = 0; i < MAXV; ++i){
+		g[i].clear();
+	}
+}
+
+void cleancolor(){
+	for(int i = 0; i < MAXV; ++i){
+		color[i] = -1;
+	}
+}
+
+void bfs(){ //runs bfs on graph and checks bicoloreability
+	queue<int> q;
+	q.push(0);
+	color[0] = 1;
+	bool coloreable = true;
+	while(!q.empty() && coloreable){
+		int t = q.front(); q.pop();
+		for(auto &a: g[t]){
+			if(color[a] == -1){
+				color[a] = 1 - color[t];	
+				q.push(a);
+			} else if(color[a] == color[t]){
+				coloreable = false;
+				cout << "NOT BICOLORABLE.\n";
+				break;
+			}
+		}
+	}
+	if(coloreable) cout << "BICOLORABLE.\n";
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	int V, E, a, b;
+	while((cin >> V)&& V){
+		cleangraph();
+		cleancolor();
+		cin >> E;
+		while(E--){
+			cin >> a >> b;
+			g[a].push_back(b);
+			g[b].push_back(a);
+		}
+		bfs();
+	}
+	return 0;
+}
