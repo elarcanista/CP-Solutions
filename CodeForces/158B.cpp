46bfdaacc06dd24221a58e6eca0a936740e4e278
Andrés Ortega <elarcanista@gmail.com>
2016-03-19 17:08:21 -0500

    Create 158B.cpp

---
+++ CodeForces/158B.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+ 
+int main() {
+	long long a, four = 0, three = 0, two= 0, one= 0;
+	cin >> a;
+	for(long long i = 0; i < a; i++){
+		int b;
+		cin >> b;
+		if(b == 4) ++four;
+		if(b == 3) ++three;
+		if(b == 2) ++two;
+		if(b == 1) ++one;
+	}
+	if(three > one){
+		one = 0;
+	}else{
+		one = one - three;
+	}
+	one += (two % 2) * 2;
+	two /= 2;
+	if(one % 4 == 0){
+		one /= 4;
+	}else{
+		one /=4;
+		++one;
+	}
+	cout << four + three + two + one;
+	return 0;
+}
