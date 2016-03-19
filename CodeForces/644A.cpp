b3c3b775a6ef68fd34ecc570fe1b44fec5839d35
Andrés Ortega <elarcanista@gmail.com>
2016-03-19 08:43:15 -0500

    644A - Parliament of Berland

---
+++ CodeForces/644A.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie();
+	int n, c, r; cin >> n >> r >> c;
+	if (n > (r*c)){ 
+		cout << -1 << "\n";
+		return 0;
+	}
+	int pair = 0, odd = -1;
+	bool d = true;
+	for(int i = 0; i < r; ++i){
+		for(int j = 0; j < c; ++j){
+			if(j) cout << " ";
+			if((i + j) % 2)
+				if(pair + 2 <= n) cout << (pair += 2);
+				else cout << 0;
+			else if (odd + 2 <= n) cout << (odd += 2);
+				else cout << 0;
+		}
+		cout << "\n";
+	}
+	return 0;
+}
