d519483aca78a25146c9242d90a4f64bda570fca
Andrés Ortega <elarcanista@gmail.com>
2016-03-19 17:03:34 -0500

    Create 630D.cpp

---
+++ CodeForces/630D.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	long long n; cin >> n;
+	cout << (n * (n+1) * 3) + 1 << "\n";
+	return 0;
+}
