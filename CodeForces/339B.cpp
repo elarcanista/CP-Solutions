5524eb523e57e9428e5ec8faa5ec60ee9492c669
Andrés Ortega <elarcanista@gmail.com>
2016-03-19 17:05:41 -0500

    Create 339B.cpp

---
+++ CodeForces/339B.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	long long n, m, in, last = 1, count = 0;
+	cin >> n >> m;
+	while(m--){
+		cin >> in;
+		if (in >= last){
+			count += in - last;
+		}else{
+			count += (n-last) + in;
+		}
+		last = in;
+	}
+	cout << count;
+	return 0;
+}
