bbfa02f83b669794fa7d62fe1e33d5eecc187e65
Andrés Ortega <elarcanista@gmail.com>
2016-03-19 17:09:02 -0500

    Create 1A.cpp

---
+++ CodeForces/1A.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+ 
+int main() {
+	long long a, b, c;
+	while((cin >> a >> b >> c)){
+		long long base = a/c;
+		if(a%c != 0){
+			++base;
+		}
+		long long height = b / c;
+		if(b % c != 0){
+			++height;
+		}
+		cout << (base * height);
+	}
+	return 0;
+}
