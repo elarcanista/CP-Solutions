ff0af043cfdcd0c122d3c4281f83b30b3afc0906
Andrés Ortega <elarcanista@gmail.com>
2016-03-30 00:55:46 -0500

    Ordering Tasks - Topological Sort BFS Kahn

--- UVa/10305.cpp
+++ UVa/10305.cpp
@@ -5,8 +5,8 @@ using namespace std;
 
 const int MAX = 1000005;
 unordered_set<int> v[MAX];
-vector<int> sorted;
-int in[MAX];
+vector<int> sorted; //stores topological sort
+int in[MAX]; //stores the income value of nodes
 int n, l;
 
 void reset(){
@@ -17,7 +17,7 @@ void reset(){
 	sorted.clear();
 }
 
-void topo(){
+void topo(){ //BFS topological sort (Kahn)
 	queue<int> q;
 	for(int i = 0; i < n; ++i){
 		//cout << i+1 << " " << in[i] << "\n";
@@ -35,19 +35,24 @@ void topo(){
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
-	while(cin >> n >> l && n && l){
+	while(cin >> n >> l && (n || l)){
 		reset();
 		while(l--){
 			int a, b; cin >> a >> b;
-			if(!v[--a].count(--b)){
+			if(!v[--a].count(--b)){ //reads an edge a single time and increments income of b
 				v[a].insert(b);
 				++in[b];
 			}
 		}
 		topo();
-		cout << sorted[0]+1;
-		for(int i = 1; i < n; ++i) cout << " " << sorted[i]+1;
-		cout  << "\n";
+		if(sorted.size()){ //prints the order
+			cout << sorted[0]+1;
+			for(int i = 1; i < n; ++i) cout << " " << sorted[i]+1;
+		}else{ //if there are no edges, then print all nodes
+			cout << 1;
+			for(int i = 2; i <=n; ++i) cout << " " << i;
+		} 
+		cout << "\n";	
 	}
 	return 0;
 }
