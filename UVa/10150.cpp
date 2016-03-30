ec173d4f2b832c9e3fcce1d637a569d9653a100e
Andrés Ortega <elarcanista@gmail.com>
2016-03-30 15:13:49 -0500

    to do**********************************************************

--- UVa/10150.cpp
+++ UVa/10150.cpp
@@ -1,9 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
 //UVa 429 - Word Transformation
-map<string, vector<string> > g;
-map<string, bool> visited;
+unordered_set<string> visited;
 map<string, string> back;
+
+struct compare {
+    bool operator()(const std::string& first, const std::string& second) {
+    	if(first.size() != second.size()) return first.size() < second.size();
+    	for(int i=0; i < first.size() && i < second.size(); ++i) {
+	        if( first[i] != second[i]) return (first[i] < second[i]);
+    	}
+    	return true;
+    }
+};
+map<string, vector<string>, compare> g;
 
 void reset(){
 	for(auto &a: back) back[a.first] = "";
@@ -30,7 +40,7 @@ void print(string a){
 bool bfs(string &a, string &b){ //runs bfs trying to reach b from a and returns distance 
 	queue<string> q;
 	q.push(a);
-	visited[a] = true;
+	visited.insert(a);
 	bool undone = true;
 	while(!q.empty() && undone){
 		string t = q.front(); q.pop();
@@ -39,10 +49,10 @@ bool bfs(string &a, string &b){ //runs bfs trying to reach b from a and returns 
 				back[c] = t;
 				return 1;	
 			} 
-			if(!visited[c]){
+			if(!visited.count(c)){
 				q.push(c);
 				back[c] = t;
-				visited[c] = true;
+				visited.insert(c);
 			}
 		}
 	}
@@ -55,23 +65,27 @@ int main() {
 	string word, b;
 	getline(cin, word);
 	while(word != ""){
+		back.clear();
 		visited.clear();
 		g.clear();
 		g[word];
 		while(getline(cin,word) && word != ""){ //fills dictionary
-			if(!g.count(word)){
-				g[word];
-				for(auto &c: g){
-					string a = c.first;
-					if(a.size() > word.size()) continue;
-					if(dif1(word, a)){
-						g[word].push_back(a);
-						g[a].push_back(word);
-					}
+			g[word];
+			for(auto &c: g){
+				string a = c.first;
+				if(a.size() > word.size()) continue;
+				if(dif1(word, a)){
+					g[word].push_back(a);
+					g[a].push_back(word);
 				}
 			}
 		}
-		getline(cin, word);
+		getline(cin, word);/*
+		for(auto &a: g){
+			cout << a.first << ":\n";
+			for(auto &b: a.second) cout << b << "\n";
+		}*/
+		cout << g.size();
 		while(word != ""){
 			reset();
 			stringstream ss(word);
@@ -79,7 +93,7 @@ int main() {
 			if(bfs(word, b)){
 				cout << word;
 				print(b);
-			}else cout << "IMPOSSIBLE";
+			}else cout << "No solution.";
 			getline(cin, word);
 			cout << "\n" << ((word != "")? "\n": "");
 			visited.clear();
