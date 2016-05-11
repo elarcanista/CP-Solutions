8619ea5e822a983b780cfbe967ea2089f2f61b65
Andrés Ortega <elarcanista@gmail.com>
2016-05-10 22:00:59 -0500

    Regular Simple Polygons / Ad-Hoc

---
+++ URI/1959.cpp
@@ -0,0 +1,15 @@
+//Author: Andrés Felipe Ortega Montoya
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> ii;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	ll a, b;
+	while(cin >> a >> b) cout << (a * b) << "\n";
+	return 0;
+}
