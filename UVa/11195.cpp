c6fc126d200b9d4c3b0814be30fe648b5c81f8dc
Andrés Ortega <elarcanista@gmail.com>
2016-08-30 10:36:13 -0500

    Another n-Queen Problem / Bitmask - Backtracking

---
+++ UVa/11195.cpp
@@ -0,0 +1,48 @@
+//Author: Andres Felipe Ortega Montoya
+//UVa 11195 - Another n-Queen Problem
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> ii;
+
+const int INFI = 1 << 30;
+const ll INFLL = 1LL << 62;
+
+vector<string> s;
+int queens;
+bitset<30> rw, ld, rd;
+
+void queen(int m, int n){
+	if(m == n){
+		++queens;
+		return;
+	}
+	for(int i = 0; i < n; ++i){
+		//if(!s.count({m, i}) && !rw[i] && !ld[m-i+n-1] && !rd[m+i]){
+		if(s[m][i] != '*' && !rw[i] && !ld[m-i+n-1] && !rd[m+i]){
+			rw[i] = ld[m-i+n-1] = rd[m+i] = true;
+			queen(m+1, n);
+			rw[i] = ld[m-i+n-1] = rd[m+i] = false;
+		}
+	}
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	int n, TC = 1;
+	while(cin >> n && n){
+		queens = 0;
+		s.clear();
+		char c;
+		string str;
+		for(int i = 0; i < n; ++i){
+			cin >> str;
+			s.push_back(str);
+		}
+		queen(0, n);
+		cout << "Case " << TC++ << ": " << queens << "\n";
+	}
+	return 0;
+}
