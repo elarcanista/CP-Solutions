e31364cffcc635c0778d04bca25caa3337d47c9b
Andrés Ortega <elarcanista@gmail.com>
2016-09-24 23:15:45 -0500

    Angry Professor / Ad-Hoc

---
+++ HackerRank/Algorithms/Implementation/angryProfessor.cpp
@@ -0,0 +1,26 @@
+//Author: Andres Felipe Ortega Montoya                                                                                                                        
+//HackerRank - Angry Professor                                                                                                                                
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> ii;
+
+const int INF = 1 << 30;
+
+int main(){
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  int TC, count = 0, in;
+  cin >> TC;
+  while(TC--){
+    int st, req;
+    cin >> st >> req;
+    while(st--){
+      cin >> in;
+      if(in <= 0) ++count;
+    }
+    cout << (count >= req? "NO": "YES") << "\n";
+    count = 0;
+  }
+}
