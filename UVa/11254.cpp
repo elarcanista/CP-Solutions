d5b21c52665a57669a62356ee6c4bc84719c8a75
elarcanista <elarcanista@gmail.com>
2017-09-08 22:06:00 -0500

    Consecutive Integers / Aritmetic progression

--- UVa/11254.cpp
+++ UVa/11254.cpp
@@ -2,21 +2,20 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+typedef long long ll;
+
 int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	double n;
-	cout << fixed << setprecision(0);
-	while(cin >> n && n != -1){
-	  //j(j+1)/2 - i(i+1)/2 + i = n
-		for(double i = 1; i <= n; ++i){ //tries values for lower bound
-			double c = 2*n + i*(i - 1); //this must be equals to j(j+1)
-			double j = (-1 + sqrt(1 + 4*c)) / 2; //calculates j from j^2 + j - c = 0
-			if(j == ceil(j)){ //if the answer is exact then it is the answer
-				cout << n << " = " << i << " + ... + " << j << "\n";
-				break;
-			}
-		}
-	}
-	return 0;
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  ll s;
+  while(cin >> s && s != -1){
+    for(ll n = sqrt(2*s); n > 0; --n){
+      ll a = (2*s - n*n + n)/(2*n);
+      if(a*n + (n*n - n)/2 == s){
+        cout << s << " = " << a << " + ... + " << a+n-1 << "\n";
+        break;
+      }
+    }
+  }
+  return 0;
 }
