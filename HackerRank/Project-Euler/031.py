cf9e310021b0be7a7cc51467e2f5f96817240c06
elarcanista <elarcanista@gmail.com>
2022-11-10 11:42:36 -0500

    031: Coin sums

---
+++ HackerRank/Project-Euler/031.py
@@ -0,0 +1,13 @@
+max_N = 10**5
+mod = 10**9 + 7
+mem = [1] * (max_N + 1)
+coins = [2, 5, 10, 20, 50, 100, 200]
+
+for c in coins:
+    for i in range(max_N + 1):
+        if c <= i:
+            mem[i] = (mem[i] + mem[i - c]) % mod
+
+for _ in range(int(input())):
+    N = int(input())
+    print(mem[N])
