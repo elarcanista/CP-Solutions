354f73e02d4a31f3551509678899240fe3f85aac
Andrés Ortega <aortega0703@gmail.com>
2023-01-13 12:13:05 -0500

    Completing a Tree (Simpler)

--- Rosalind/tree.py
+++ Rosalind/tree.py
@@ -1,31 +1,9 @@
 import sys
-import queue
 
-def BFS(start):
-    q = queue.Queue()
-    q.put(start)
-    while not q.empty():
-        u = q.get()
-        if component[u] != -1:
-            continue
-        component[u] = start
-        for v in G[u]:
-            q.put(v)
+E = int(input())
+V = 0
 
-N = int(input())
-
-G = [[] for _ in range(N)]
 for line in sys.stdin:
-    u, v = map(int, line.split())
-    G[u-1].append(v-1)
-    G[v-1].append(u-1)
+    V += 1
 
-component = [-1] * N
-ans = 0
-
-for u in range(N):
-    if component[u] == -1:
-        BFS(u)
-        ans += 1
-
-print(ans - 1)
+print(E-1-V)
