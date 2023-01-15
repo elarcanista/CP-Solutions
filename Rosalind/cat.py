2259fbd61f743567e3c177d27ccc476d5df5949e
Andrés Ortega <aortega0703@gmail.com>
2023-01-15 16:36:00 -0500

    Catalan Numbers and RNA Secondary Structures

---
+++ Rosalind/cat.py
@@ -0,0 +1,26 @@
+import sys
+
+pairs = {"A": "U", "C": "G", "G": "C", "U": "A"}
+
+def read_input():
+    input()
+    current_DNA = ""
+    for line in sys.stdin:
+        current_DNA += line.strip()
+    return current_DNA
+
+def noncrossing(DNA):
+    if DNA in memo:
+        return memo[DNA]
+    ans = 0
+    for i in range(1, len(DNA)):
+        if pairs[DNA[0]] == DNA[i]:
+            inside = noncrossing(DNA[1:i])
+            outside = noncrossing(DNA[i+1:])
+            ans += inside * outside
+    memo[DNA] = ans
+    return ans
+
+memo = {"": 1}
+DNA = read_input()
+print(noncrossing(DNA) % 1000000)
