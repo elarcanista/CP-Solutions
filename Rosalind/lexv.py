f5c70193ac3178b0086dc41b3f6e9c452ee229bb
Andrés Ortega <aortega0703@gmail.com>
2023-01-17 17:16:43 -0500

    Ordering Strings of Varying Length Lexicographically

---
+++ Rosalind/lexv.py
@@ -0,0 +1,14 @@
+import itertools as iter
+alphabet = input().split()
+N = int(input())
+
+ans = []
+
+def all_iters(prev):
+    if len(prev) >= N:
+        return
+    for c in alphabet:
+        print(prev + c)
+        all_iters(prev + c)
+
+all_iters("")
