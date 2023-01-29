9e891d2a42627741d26bd7d6680a5bd265608228
Andrés Ortega <aortega0703@gmail.com>
2023-01-29 15:37:01 -0500

    Matryoshkas

--- CodeForces/1790D.py
+++ CodeForces/1790D.py
@@ -1,12 +1,16 @@
 for _ in range(int(input())):
     N = int(input())
-    M = list(map(int, input().split()))
+    M = sorted(list(map(int, input().split())))
     quantity = {}
+    sizes = []
+    last = -1
     for m in M:
-        if m not in quantity:
-            quantity[m] = 0
-        quantity[m] += 1
-    sizes = sorted(list(quantity.keys()))
+        if last != m:
+            sizes.append(m)
+            quantity[m] = 1
+        else:
+            quantity[m] += 1
+        last = m
     ans = quantity[sizes[0]]
     for i in range(1, len(sizes)):
         if sizes[i-1] == sizes[i] - 1:
