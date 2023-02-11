32ee9c88d20b8ae80394802a4127d32a462f4e1c
Andrés Ortega <aortega0703@gmail.com>
2023-02-10 20:34:04 -0500

    Counting Valleys

---
+++ HackerRank/Challenges/counting-valleys.py
@@ -0,0 +1,12 @@
+N = int(input())
+S = input()
+height = 0
+ans = 0
+for s in S:
+    if s == "U":
+        height += 1
+        if height == 0:
+            ans += 1
+    else:
+        height -= 1
+print(ans)
