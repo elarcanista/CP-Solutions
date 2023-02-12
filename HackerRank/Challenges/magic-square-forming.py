091f14cb1efcfcb6929824d77d4ea769ee0c0847
Andrés Ortega <aortega0703@gmail.com>
2023-02-11 21:53:28 -0500

    Forming a Magic Square

---
+++ HackerRank/Challenges/magic-square-forming.py
@@ -0,0 +1,24 @@
+import itertools as iter
+
+def magic(m):
+    H = sum(m[0:3]) == 15 and sum(m[3:6]) == 15 and sum(m[6:9]) == 15
+    V = sum(m[0::3]) == 15 and sum(m[1::3]) == 15 and sum(m[2::3]) == 15
+    D = m[0] + m[4] + m[8] == 15 and m[2] + m[4] + m[6] == 15
+    return H and V and D
+
+def cost(A, B):
+    ans = 0
+    for a, b in zip(A, B):
+        ans += abs(a - b)
+    return ans
+
+A = list(map(int, input().split()))
+A += list(map(int, input().split()))
+A += list(map(int, input().split()))
+
+ans = 9*9
+for B in iter.permutations(range(1, 10)):
+    if magic(B):
+        ans = min(ans, cost(A, B))
+
+print(ans)
