0d1d68a40962d8cf32a740561504012ef8cfd12a
Andrés Ortega <aortega0703@gmail.com>
2023-02-13 16:43:01 -0500

    Sherlock and Squares

---
+++ HackerRank/Challenges/sherlock-and-squares.py
@@ -0,0 +1,9 @@
+import math
+for _ in range(int(input())):
+    a, b = map(int, input().split())
+    n = math.ceil(a**(1/2))
+    ans = 0
+    while n**2 <= b:
+        ans += 1
+        n += 1
+    print(ans)
