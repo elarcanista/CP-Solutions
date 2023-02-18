ebc50fbcf0844714d8c1f4f7b343c9cbce4ab433
Andrés Ortega <aortega0703@gmail.com>
2023-02-18 11:16:54 -0500

    Organizing Containers of Balls

---
+++ HackerRank/Challenges/organizing-containers-of-balls.py
@@ -0,0 +1,14 @@
+for _ in range(int(input())):
+    M = []
+    for _ in range(int(input())):
+        M.append(list(map(int, input().split())))
+    sum_cols = []
+    for c in range(len(M[0])):
+        sum_cols.append(sum([r[c] for r in M]))
+    sum_rows = []
+    for r in M:
+        sum_rows.append(sum(r))
+    if sorted(sum_cols) == sorted(sum_rows):
+        print("Possible")
+    else:
+        print("Impossible")
