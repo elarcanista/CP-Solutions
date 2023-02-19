f7af88df101993d887ca84c313349ec742685f4e
Andrés Ortega <aortega0703@gmail.com>
2023-02-19 09:02:54 -0500

    Encryption

---
+++ HackerRank/Challenges/encryption.py
@@ -0,0 +1,15 @@
+import math
+S = input()
+C = math.ceil(len(S) ** (1/2))
+R = math.ceil(len(S) / C)
+M = []
+for r in range(R):
+    M.append(S[r*C: r*C + R + 1])
+
+M[-1] += (" " * (C - len(M[-1])))
+
+ans = []
+for c in range(C):
+    ans.append("".join([r[c] for r in M]).strip())
+    
+print(*ans)
