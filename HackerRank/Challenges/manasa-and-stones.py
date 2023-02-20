5f8e7795435e944f775e6f00e0b7f7231a64ca65
Andrés Ortega <aortega0703@gmail.com>
2023-02-20 10:47:51 -0500

    Manasa and Stones

---
+++ HackerRank/Challenges/manasa-and-stones.py
@@ -0,0 +1,8 @@
+import itertools as iter
+
+for _ in range(int(input())):
+    N = int(input())
+    a = int(input())
+    b = int(input())
+    a, b = sorted([a, b])
+    print(*list(set(i * b + (N - i - 1) * a for i in range(N))))
