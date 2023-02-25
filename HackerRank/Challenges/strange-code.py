a99452aeb14eb9693789cd4596b18c046897d93f
Andrés Ortega <aortega0703@gmail.com>
2023-02-25 09:37:30 -0500

    Strange Counter

---
+++ HackerRank/Challenges/strange-code.py
@@ -0,0 +1,9 @@
+import math
+
+N = int(input())
+layer = lambda N : math.ceil(math.log2(N/3 + 1) - 1)
+dist = lambda N: N - 3*(2**(layer(N)) - 1) - 1
+init = lambda N: 3*2**layer(N)
+ans = lambda N: init(N) - dist(N)
+
+print(ans(N))
